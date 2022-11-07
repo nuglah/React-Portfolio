3dc350ae922de7c0ef60824ca4a6745f7f6d62f9
Ryan <thisianemailaddressthatis@gmail.com>
2022-11-07 14:30:33 -0600

    Updates

---
+++ precache-manifest.9435988ed9926e4028269881277aaf3a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c59d70b7f6b3d8a8241393c17c6717b",
+    "url": "/week20-react-portfolio/index.html"
+  },
+  {
+    "revision": "6d6b6af1756be06596c5",
+    "url": "/week20-react-portfolio/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "adc5cd720e4a127c1339",
+    "url": "/week20-react-portfolio/static/css/main.6ebdb4d0.chunk.css"
+  },
+  {
+    "revision": "6d6b6af1756be06596c5",
+    "url": "/week20-react-portfolio/static/js/2.833ffa5f.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/week20-react-portfolio/static/js/2.833ffa5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc5cd720e4a127c1339",
+    "url": "/week20-react-portfolio/static/js/main.87892645.chunk.js"
+  },
+  {
+    "revision": "8b65dac26fe4f4867d46",
+    "url": "/week20-react-portfolio/static/js/runtime-main.8a13dafc.js"
+  },
+  {
+    "revision": "377aef5dc2dfb6c73c7568c9d3024677",
+    "url": "/week20-react-portfolio/static/media/Memorize.377aef5d.png"
+  },
+  {
+    "revision": "4e4420faf0dcd82104e77d75af0bd050",
+    "url": "/week20-react-portfolio/static/media/Notes.4e4420fa.png"
+  },
+  {
+    "revision": "e1497e86a7b7bf375c2f9a3359b22501",
+    "url": "/week20-react-portfolio/static/media/Social.e1497e86.png"
+  },
+  {
+    "revision": "82b4ce4f1cba110cf2d0c44d9b8a3027",
+    "url": "/week20-react-portfolio/static/media/Weather.82b4ce4f.png"
+  },
+  {
+    "revision": "8af9bc91b8682d4d237522db231103ad",
+    "url": "/week20-react-portfolio/static/media/Work.8af9bc91.png"
+  },
+  {
+    "revision": "219f4b65922fa81c3205f8afd8f5aa05",
+    "url": "/week20-react-portfolio/static/media/abstract.219f4b65.jpg"
+  },
+  {
+    "revision": "b88ddd455c3521294550b99d1d5184cd",
+    "url": "/week20-react-portfolio/static/media/unburied.b88ddd45.png"
+  }
+]);
