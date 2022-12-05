d4d4685943e6c29eaa2ca7d2ca84486bdd41e88d
nuglah <thisisanemailaddressthatis@gmail.com>
2022-12-05 10:08:51 -0600

    Updates

---
+++ precache-manifest.a31e31f70eb7d6c773867ed9b7ac639f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edb96ef846b2771896f4096eb5f2c61",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "d9449f4adf66e342e4dc",
+    "url": "/React-Portfolio/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "321a3c2b6eaa492d2ded",
+    "url": "/React-Portfolio/static/css/main.3ea6c3fa.chunk.css"
+  },
+  {
+    "revision": "d9449f4adf66e342e4dc",
+    "url": "/React-Portfolio/static/js/2.b0073e95.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/React-Portfolio/static/js/2.b0073e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321a3c2b6eaa492d2ded",
+    "url": "/React-Portfolio/static/js/main.60f2a2ce.chunk.js"
+  },
+  {
+    "revision": "44c7ebc3cfea037ef8d9",
+    "url": "/React-Portfolio/static/js/runtime-main.cb3cf7cc.js"
+  },
+  {
+    "revision": "4e4420faf0dcd82104e77d75af0bd050",
+    "url": "/React-Portfolio/static/media/Notes.4e4420fa.png"
+  },
+  {
+    "revision": "e1497e86a7b7bf375c2f9a3359b22501",
+    "url": "/React-Portfolio/static/media/Social.e1497e86.png"
+  },
+  {
+    "revision": "6838908c7a430ac8c5d6aa5dce3bae89",
+    "url": "/React-Portfolio/static/media/Tutorial.6838908c.png"
+  },
+  {
+    "revision": "82b4ce4f1cba110cf2d0c44d9b8a3027",
+    "url": "/React-Portfolio/static/media/Weather.82b4ce4f.png"
+  },
+  {
+    "revision": "8af9bc91b8682d4d237522db231103ad",
+    "url": "/React-Portfolio/static/media/Work.8af9bc91.png"
+  },
+  {
+    "revision": "219f4b65922fa81c3205f8afd8f5aa05",
+    "url": "/React-Portfolio/static/media/abstract.219f4b65.jpg"
+  },
+  {
+    "revision": "b88ddd455c3521294550b99d1d5184cd",
+    "url": "/React-Portfolio/static/media/unburied.b88ddd45.png"
+  }
+]);
